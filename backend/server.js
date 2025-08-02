bd4dfd6bccdc54dc29bea6dd0e02ba7fb6632135
Gustav Sundin <sundin666@gmail.com>
2025-08-02 15:09:54 +0200

    Use port provided by Heroku, or 3001 locally

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,8 @@ app.post('/user/:id/adventures', async (req, res) => {
 });
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
